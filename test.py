54282a899cdd59651cc9bf93f14ce4eb08161524
safrin <mylifeofjava@gmail.com>
2022-08-07 17:35:28 +0530

    t

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hell");
+class Post:
+    def __init__(self,title,description):
+        self.title=title
+        self.description=description
+
+    def print(self):
+        print("Title "+self.title)
+        print("Desc "+self.description)
+
+
+post=Post("Safrin","Critical Recource")
+
+post.print()
